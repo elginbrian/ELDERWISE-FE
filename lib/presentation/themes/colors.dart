0e552fb760ccb912c7f6a909d63ef44feb16cb69
Rizqi <rizqiaditya222@gmail.com>
2025-04-04 14:13:24 +0700

    login screen

---
+++ lib/presentation/themes/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Neutral Colors
+  static const Color neutral10 = Color(0xFFFFFFFF);
+  static const Color neutral20 = Color(0xFFEBF0F4);
+  static const Color neutral30 = Color(0xFFE4E8EC);
+  static const Color neutral40 = Color(0xFFD7DCDf);
+  static const Color neutral50 = Color(0xFFBABEC1);
+  static const Color neutral60 = Color(0xFF98989D);
+  static const Color neutral70 = Color(0xFF707375);
+  static const Color neutral80 = Color(0xFF5D5F61);
+  static const Color neutral90 = Color(0xFF3D3F40);
+  static const Color neutral100 = Color(0xFF0A0A0A);
+
+  // Primary Colors
+  static const Color primaryMain = Color(0xFFFFDB00);
+  static const Color primarySurface = Color(0xFFFFFEF7);
+  static const Color primaryBorder = Color(0xFFFDFCF1);
+  static const Color primaryHover = Color(0xFFFFF741);
+  static const Color primaryPressed = Color(0xFFD1A100);
+  static const Color primaryFocus = Color(0xFFFFFEF7);
+}
